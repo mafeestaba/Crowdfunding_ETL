f8afc40d8793d0fa47ca07ab3dfe2ad2888d9829
Jose <jcuevas9328@gmail.com>
2024-03-14 19:41:02 -0500

    final push

---
+++ Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/FpSdGE
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "capaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" text   NOT NULL,
+    "description" text   NOT NULL,
+    "goal" numeric   NOT NULL,
+    "pledged" numeric   NOT NULL,
+    "outcome" text   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" text   NOT NULL,
+    "currency" text   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" text   NOT NULL,
+    "subcategory_id" text   NOT NULL,
+    CONSTRAINT "pk_capaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" text   NOT NULL,
+    "category" text   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" text   NOT NULL,
+    "subcategory" text   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" text   NOT NULL,
+    "last_name" text   NOT NULL,
+    "email" text   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "capaign" ADD CONSTRAINT "fk_capaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "capaign" ADD CONSTRAINT "fk_capaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "capaign" ADD CONSTRAINT "fk_capaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
